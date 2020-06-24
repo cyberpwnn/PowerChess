07fb250bb7997dfd83dfbd3cb507e27262ab4a5f
Daniel Mills <danielmillst@gmail.com>
2020-06-23 21:24:33 -0400

    Proj

---
+++ app/lib/home.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class PowerChess extends StatefulWidget {
+  PowerChess({Key key}) : super(key: key);
+
+  @override
+  _PowerChessState createState() => _PowerChessState();
+}
+
+class _PowerChessState extends State<PowerChess> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Text("Derp")
+    );
+  }
+}
---
+++ app/lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:app/home.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Power Chess',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: PowerChess(),
+    );
+  }
+}
